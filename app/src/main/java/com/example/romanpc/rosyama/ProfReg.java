52414648694463eb8f2d2352c5c5647d57b7b9fe
romanrr <romandauria@gmail.com>
2018-01-09 13:58:11 +0600

    Создали окно входа

---
+++ app/src/main/java/com/example/romanpc/rosyama/ProfReg.java
@@ -0,0 +1,13 @@
+package com.example.romanpc.rosyama;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfReg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prof_reg);
+    }
+}
