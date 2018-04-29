2982107801e72a2acd7ea558f7f49f310f30f100
romanrr <romandauria@gmail.com>
2018-04-29 12:37:18 +0600

    фиксы

--- app/src/main/java/com/example/romanpc/rosyama/MainActivity.java
+++ app/src/main/java/com/example/romanpc/rosyama/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.romanpc.rosyama;
 
 import android.Manifest;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomSheetBehavior;
@@ -25,6 +28,7 @@ import android.widget.Toast;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingEvent;
 import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
@@ -35,18 +39,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, OnCompleteListener<Void> {
 
     private GoogleMap mMap;
     private static final int PERMISSION_REQUEST_FOR_GET_USER_LOCATION = 1;
     FloatingActionButton fab;
-    private GeofencingClient mGeofencingClient;
     private ArrayList<Geofence> mGeofenceList;
     private PendingIntent mGeofencePendingIntent;
 
@@ -135,8 +141,28 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
 
-        mGeofencingClient = LocationServices.getGeofencingClient(this);
         mGeofenceList = new ArrayList<>();
+
+        GeofencingClient client = LocationServices.getGeofencingClient(this);
+
+        GeofencingRequest.Builder geofencingRequestBuilder = new GeofencingRequest.Builder();
+        geofencingRequestBuilder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER | GeofencingRequest.INITIAL_TRIGGER_DWELL);
+
+//        Geofence.Builder geofenceBuilder = new Geofence.Builder();
+//        geofenceBuilder.setCircularRegion(55.041082, 73.323116, 100);
+//        geofenceBuilder.setRequestId("111");
+//        geofenceBuilder.setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER);
+//        geofenceBuilder.setExpirationDuration(Geofence.NEVER_EXPIRE);
+//        Geofence geofence = geofenceBuilder.build();
+
+//        GeofencingRequest.Builder addGeofence = geofencingRequestBuilder.addGeofence(geofence);
+//        GeofencingRequest geofencingRequest = addGeofence.build();
+
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+//        client.addGeofences(geofencingRequest, getGeofencePendingIntent())
+//                .addOnCompleteListener(this);
 
         final DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
         ArrayList<HashMap<String, String>> listPits = dataBaseHelper.getPits();
@@ -146,30 +172,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             double lng = Double.parseDouble(listPits.get(i).get("Lng"));
             Marker marker = mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)));
             marker.setTag(listPits.get(i).get("_id"));
-            mGeofenceList.add(new Geofence.Builder()
-                    .setRequestId(listPits.get(i).get("_id"))
-                    .setCircularRegion(lat, lng, 100)
-                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
-                    .setExpirationDuration(-1)
-                    .build());
+            Geofence.Builder geofenceBuilder = new Geofence.Builder();
+            geofenceBuilder.setCircularRegion(lat, lng, 100);
+            geofenceBuilder.setRequestId(listPits.get(i).get("_id"));
+            geofenceBuilder.setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER);
+            geofenceBuilder.setExpirationDuration(Geofence.NEVER_EXPIRE);
+            Geofence geofence = geofenceBuilder.build();
+            mGeofenceList.add(geofence);
             i++;
         }
+        GeofencingRequest.Builder addGeofence = geofencingRequestBuilder.addGeofences(mGeofenceList);
+        GeofencingRequest geofencingRequest = addGeofence.build();
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        mGeofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent())
-                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(MainActivity.this, "Успешно добавлено!", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(this, new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                    }
-                });
-
+        client.addGeofences(geofencingRequest, getGeofencePendingIntent())
+                .addOnCompleteListener(this);
 
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
@@ -204,6 +223,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 });
     }
 
+    private PendingIntent getGeofencePendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+        if (mGeofencePendingIntent != null) {
+            return mGeofencePendingIntent;
+        }
+        Intent intent = new Intent(this, GeofenceBroadcastReceiver.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
+        // addGeofences() and removeGeofences().
+        mGeofencePendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return mGeofencePendingIntent;
+    }
+
     protected void createLocationRequest() {
         LocationRequest mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(1500);
@@ -211,19 +242,57 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
-    private PendingIntent getGeofencePendingIntent() {
-        if (mGeofencePendingIntent != null) {
-            return mGeofencePendingIntent;
+
+    @Override
+    public void onComplete(@NonNull Task<Void> task) {
+        if (task.isSuccessful()) {
+            Toast.makeText(this, "AAAAAA", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "BBBBBB", Toast.LENGTH_SHORT).show();
         }
-        Intent intent = new Intent(this, GeofenceBroadcastReceiver.class);
-        mGeofencePendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        return mGeofencePendingIntent;
     }
 
-    private GeofencingRequest getGeofencingRequest() {
-        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
-        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
-        builder.addGeofences(mGeofenceList);
-        return builder.build();
+    public static class GeofenceBroadcastReceiver extends BroadcastReceiver implements TextToSpeech.OnInitListener, TextToSpeech.OnUtteranceCompletedListener {
+        TextToSpeech textToSpeech;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            textToSpeech = new TextToSpeech(context, this);
+            if(!textToSpeech.isSpeaking()){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    textToSpeech.speak("Внимание! Впереди яма", textToSpeech.QUEUE_ADD, null, "1");
+                }else{
+                    HashMap<String, String> stringStringHashMap = new HashMap<>();
+                    stringStringHashMap.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "Внимание! Впереди яма.");
+                    textToSpeech.speak("Внимание! Впереди яма", textToSpeech.QUEUE_ADD, stringStringHashMap);
+                }
+            }else{
+                onDestroy();
+            }
+            Toast.makeText(context, "Вы в зоне", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onInit(int status) {
+            textToSpeech.setOnUtteranceCompletedListener(this);
+        }
+
+        @Override
+        public void onUtteranceCompleted(String utteranceId) {
+//            runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    Toast.makeText(, "Completed!", Toast.LENGTH_SHORT).show();
+//                }
+//            });
+        }
+
+        protected void onDestroy(){
+            if(textToSpeech != null){
+                textToSpeech.stop();
+                textToSpeech.shutdown();
+                textToSpeech = null;
+            }
+        }
     }
 }
